f502b35bd3b2a278ae6d72b7a931adb66bfc9a62
Engel Javier Pinto Ruiz <engeljavierpinto@gmail.com>
2020-12-16 13:26:34 -0400

    chore: Agregué page_objects e hice refactor en algunos tests.

--- apps/features/steps/ir_a_encuesta_steps.py
+++ apps/features/steps/ir_a_encuesta_steps.py
@@ -4,6 +4,8 @@ from behave import given, when, then
 
 from apps.polls.factories import QuestionFactory, ChoiceFactory
 
+from pages import HomePage, PollPage
+
 
 @given('que hay una encuesta llamada {name}')
 def step_impl(context, name):
@@ -12,24 +14,21 @@ def step_impl(context, name):
 
 @given('le agregaron las opciones {si} y {no}')
 def step_impl(context, si, no):
-
     ChoiceFactory.create(text=si, poll=context.poll)
     ChoiceFactory.create(text=no, poll=context.poll)
 
 
 @when('vaya a la encuesta')
 def step_impl(context):
-
-    context.browser.get(context.get_url(reverse('polls:index')))
-
-    context.browser.find_element_by_css_selector('a[data-encuesta]').click()
+    context.home_page = HomePage(context.browser)
+    context.home_page.get(context.get_url(reverse('polls:index')))
+    context.home_page.link_to_poll.click()
 
 
 @then('podré ver sus opciones {si} y {no}')
 def step_impl(context, si, no):
-
-    opciones = context.browser\
-        .find_element_by_css_selector('input[type="radio"]').text
+    poll_page = PollPage(context.browser)
+    opciones = poll_page.options.text
 
     context.test.assertIn(opciones, si)
     context.test.assertIn(opciones, no)
